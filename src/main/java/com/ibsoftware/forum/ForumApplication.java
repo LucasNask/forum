34f3db01c01554b124fba4fd2b7920f63700d31b
Lucas Nascimento <Lucas_nascimento1@hotmail.com>
2022-03-29 18:03:18 -0300

    build war

--- src/main/java/com/ibsoftware/forum/ForumApplication.java
+++ src/main/java/com/ibsoftware/forum/ForumApplication.java
@@ -2,16 +2,24 @@ package com.ibsoftware.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
 @EnableCaching
-public class ForumApplication {
+//Caso queira gerar um WAR ao inves do Jar adicionar o extends SpringBootServletInitializer
+public class ForumApplication /* extends SpringBootServletInitializer */ {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
 	}
 
+	//Caso queira gerar um WAR ao inves do Jar descomentar o metodo de baixo
+	/* @Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ForumApplication.class);
+	} */
 }
